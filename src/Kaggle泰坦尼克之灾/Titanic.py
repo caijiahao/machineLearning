570b17fb46f41dd3077335406e12310609fd1f4e
stevenfen <634606228@qq.com>
2016-11-20 22:01:58 +0800

    kaggle泰坦题目（一）

---
+++ src/Kaggle泰坦尼克之灾/Titanic.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version: ??
+@author: phpergao
+@license: Apache Licence 
+@file: Titanic.py
+@time: 2016/11/20 22:00
+"""
+import pandas as pd #数据分析
+import numpy as np #科学计算
+from pandas import Series,DataFrame
+
+data_train = pd.read_csv("train.csv")
+print  data_train.describe()
