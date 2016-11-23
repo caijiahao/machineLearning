5d37c05e5d4e0f1938192b589605b247f129fc10
stevenfen <634606228@qq.com>
2016-11-23 17:11:57 +0800

    kaggle泰坦题目（四）

--- src/Kaggle泰坦尼克之灾/Titanic.py
+++ src/Kaggle泰坦尼克之灾/Titanic.py
@@ -122,6 +122,10 @@ g = data_train.groupby(['SibSp','Survived'])
 df = pd.DataFrame(g.count()['PassengerId'])
 #print df
 
+#ticket是船票编号，应该是unique的，和最后的结果没有太大的关系，先不纳入考虑的特征范畴把
+#cabin只有204个乘客有值，我们先看看它的一个分布
+print data_train.Cabin.value_counts()
+
 fig = plt.figure()
 fig.set(alpha=0.2)  # 设定图表颜色alpha参数
 
@@ -132,7 +136,7 @@ df.plot(kind='bar', stacked=True)
 plt.title(u"按Cabin有无看获救情况")
 plt.xlabel(u"Cabin有无")
 plt.ylabel(u"人数")
-plt.show()
+#plt.show()
 
 from sklearn.ensemble import RandomForestRegressor
 
@@ -171,7 +175,6 @@ def set_Cabin_type(df):
 
 data_train, rfr = set_missing_ages(data_train)
 data_train = set_Cabin_type(data_train)
-#print data_train
 
 dummies_Cabin = pd.get_dummies(data_train['Cabin'], prefix= 'Cabin')
 
@@ -183,7 +186,6 @@ dummies_Pclass = pd.get_dummies(data_train['Pclass'], prefix= 'Pclass')
 
 df = pd.concat([data_train, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
 df.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
-#print df
 
 import sklearn.preprocessing as preprocessing
 scaler = preprocessing.StandardScaler()
@@ -191,4 +193,48 @@ age_scale_param = scaler.fit(df['Age'])
 df['Age_scaled'] = scaler.fit_transform(df['Age'], age_scale_param)
 fare_scale_param = scaler.fit(df['Fare'])
 df['Fare_scaled'] = scaler.fit_transform(df['Fare'], fare_scale_param)
-print df
+
+from sklearn import linear_model
+
+# 用正则取出我们要的属性值
+train_df = df.filter(regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+train_np = train_df.as_matrix()
+
+# y即Survival结果
+y = train_np[:, 0]
+
+# X即特征属性值
+X = train_np[:, 1:]
+
+# fit到RandomForestRegressor之中
+clf = linear_model.LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
+clf.fit(X, y)
+print clf
+
+data_test = pd.read_csv("test.csv")
+data_test.loc[ (data_test.Fare.isnull()), 'Fare' ] = 0
+# 接着我们对test_data做和train_data中一致的特征变换
+# 首先用同样的RandomForestRegressor模型填上丢失的年龄
+tmp_df = data_test[['Age','Fare', 'Parch', 'SibSp', 'Pclass']]
+null_age = tmp_df[data_test.Age.isnull()].as_matrix()
+# 根据特征属性X预测年龄并补上
+X = null_age[:, 1:]
+predictedAges = rfr.predict(X)
+data_test.loc[ (data_test.Age.isnull()), 'Age' ] = predictedAges
+
+data_test = set_Cabin_type(data_test)
+dummies_Cabin = pd.get_dummies(data_test['Cabin'], prefix= 'Cabin')
+dummies_Embarked = pd.get_dummies(data_test['Embarked'], prefix= 'Embarked')
+dummies_Sex = pd.get_dummies(data_test['Sex'], prefix= 'Sex')
+dummies_Pclass = pd.get_dummies(data_test['Pclass'], prefix= 'Pclass')
+
+
+df_test = pd.concat([data_test, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
+df_test.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
+df_test['Age_scaled'] = scaler.fit_transform(df_test['Age'], age_scale_param)
+df_test['Fare_scaled'] = scaler.fit_transform(df_test['Fare'], fare_scale_param)
+
+test = df_test.filter(regex='Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+predictions = clf.predict(test)
+result = pd.DataFrame({'PassengerId':data_test['PassengerId'].as_matrix(), 'Survived':predictions.astype(np.int32)})
+result.to_csv("logistic_regression_predictions.csv", index=False)
