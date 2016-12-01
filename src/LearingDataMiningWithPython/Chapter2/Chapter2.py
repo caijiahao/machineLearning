1069031bd222a9844ffddb3fe7d1ac2f8fb8326e
caijiahao <634606228@qq.com>
2016-12-01 21:41:13 +0800

    Python数据挖掘入门与时间练习代码（二）

--- src/LearingDataMiningWithPython/Chapter2/Chapter2.py
+++ src/LearingDataMiningWithPython/Chapter2/Chapter2.py
@@ -5,3 +5,44 @@
 # @Site    : 
 # @File    : Chapter2.py
 # @Software: PyCharm
+
+#导入numpy库和csv库
+import numpy as np
+import csv
+
+data_filename = "ionosphere.data"
+
+#创建Numpy数组xH和y存放的数据集
+x=np.zeros((351,34),dtype=float)
+y=np.zeros((351,),dtype=bool)
+
+#用csv模式导入数据集
+with open(data_filename,'r') as input_file:
+   reader = csv.reader(input_file)
+   for i,row in enumerate(reader):
+       data = [float(datum) for datum in row[:-1]]
+       x[i] = data
+       y[i] = row[-1] == 'g'
+
+#sciket-learning将数据库分为训练集和测试集的函数
+from sklearn.cross_validation import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(x,y,random_state=14)
+
+#初始化K近邻分类器
+from sklearn.neighbors import KNeighborsClassifier
+estimator = KNeighborsClassifier()
+estimator.fit(X_train,y_train)
+
+y_predicted = estimator.predict(X_test)
+accuracy = np.mean(y_predicted == y_test) *100
+#print accuracy
+#print "The test accuracy is {:.1f}%".format(accuracy)
+
+#交叉检验
+from sklearn.cross_validation import cross_val_score
+scores = cross_val_score(estimator,x,y,scoring='accuracy')
+average_accuracy = np.mean(scores) *100
+#print accuracy
+#print "The test average_accuracy is {:.1f}%".format(average_accuracy)
+
+#测试一下一系列n_neighbors的效果
