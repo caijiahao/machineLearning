4a2ccd124e6042f85c0faeaea9421595511c2f7c
caijiahao <634606228@qq.com>
2016-12-02 14:56:36 +0800

    Python数据挖掘入门与时间练习代码（四）

--- src/LearingDataMiningWithPython/Chapter3/Chapter3.py
+++ src/LearingDataMiningWithPython/Chapter3/Chapter3.py
@@ -15,4 +15,24 @@ data_filename = "nba.csv"
 dataset = pd.read_csv(data_filename,parse_dates=["Date"])
 dataset.columns = ["Date","Start(ET)","Visitor Team","VisitorPts","Home Team","HomePts","Score Type","OT?","Notes"]
 #输出数据前五行
+#print dataset.ix[:5]
+
+#提取新特征
+dataset["HomeWin"] = dataset["VisitorPts"] < dataset["HomePts"]
+y_true = dataset["HomeWin"].values
+
+#储存上次比赛的结果
+from collections import defaultdict
+won_last = defaultdict(int)
 print dataset.ix[:5]
+for index,row in dataset.iterrows():
+    home_team = row["Home Team"]
+    visitor_team = row["Visitor Team"]
+    row["HomeLastWin"] = won_last[home_team]
+    row["VisitorLastWin"] = won_last[visitor_team]
+    dataset.ix[index] = row
+    won_last[home_team] = row["HomeWin"]
+    won_last[visitor_team] = not row["HomeWin"]
+#print dataset.ix[20:25]
+
+
