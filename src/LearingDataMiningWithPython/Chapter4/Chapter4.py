4e9dc9cc5a1c5c15b66a3a556f458fc9dd9820d0
caijiahao <634606228@qq.com>
2016-12-05 14:57:57 +0800

    Python数据挖掘入门与时间练习代码（四）Aprior实现代码最终版本

--- src/LearingDataMiningWithPython/Chapter4/Chapter4.py
+++ src/LearingDataMiningWithPython/Chapter4/Chapter4.py
@@ -10,8 +10,8 @@
 import pandas as pd
 import sys
 
-ratings_filename = "ratings.csv"
-all_ratings = pd.read_csv(ratings_filename,delimiter=",",header=None,names=["UserID","MovieID","Rating","Timestamp"])
+ratings_filename = "u.data"
+all_ratings = pd.read_csv(ratings_filename,delimiter="\t",header=None,names=["UserID","MovieID","Rating","Timestamp"])
 all_ratings["Timestamp"] = pd.to_datetime(all_ratings['Timestamp'],unit = 's')
 #print all_ratings[:5]
 
@@ -58,11 +58,90 @@ for k in range(2,20):
 
 #删除只有一个的频繁集
 del frequent_itemsets[1]
-print frequent_itemsets[10]
+#print frequent_itemsets[10]
 
+#制造备选规则
+candidate_rules = []
+for itemset_length,itemset_counts in frequent_itemsets.items():
+    for itemset in itemset_counts.keys():
+        for conclusion in itemset:
+            premise = itemset - set((conclusion,))
+            candidate_rules.append((premise,conclusion))
+#print candidate_rules[:5]
 
+#计算每条规则的置信度
+correct_counts = defaultdict(int)
+incorrect_counts = defaultdict(int)
+for user, reviews in favorable_reviews_by_users.items():
+    for candidate_rule in candidate_rules:
+        premise, conclusion = candidate_rule
+        if premise.issubset(reviews):
+            if conclusion in reviews:
+                correct_counts[candidate_rule] += 1
+            else:
+                incorrect_counts[candidate_rule] += 1
+rule_confidence = {candidate_rule: correct_counts[candidate_rule] / float(correct_counts[candidate_rule] + incorrect_counts[candidate_rule]) for candidate_rule in candidate_rules}
 
+#对置信字典进行排序，输出置信度最高的前五条规则
+from operator import itemgetter
+sorted_confidence = sorted(rule_confidence.items(), key=itemgetter(1), reverse=True)
+for index in range(5):
+    print "Rule #{0}".format(index + 1)
+    (premise, conclusion) = sorted_confidence[index][0]
+    print "Rule: If a person recommends {0} they will also recommend {1}".format(premise, conclusion)
+    print " - Confidence: {0:.3f}".format(rule_confidence[(premise, conclusion)])
+    print " "
 
+#电影名称文件名
+movie_name_filename = "u.item"
+movie_name_data  = pd.read_csv(movie_name_filename,delimiter="|",encoding="mac-roman")
+movie_name_data.columns = ["MovieID", "Title", "Release Date", "Video Release", "IMDB", "<UNK>", "Action", "Adventure",
+                           "Animation", "Children's", "Comedy", "Crime", "Documentary", "Drama", "Fantasy", "Film-Noir",
+                           "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"]
+#print movie_name_data[:5]
 
+#创建找到电影名称的函数
+def get_movie_name(movie_id):
+    title_object = movie_name_data[movie_name_data["MovieID"] == movie_id]["Title"]
+    title = title_object.values[0]
+    return title
 
+#加上movie正式名称后的输出
+for index in range(4,9):
+    print "Rule #{0}".format(index + 1)
+    (premise, conclusion) = sorted_confidence[index][0]
+    premise_names = ", ".join(get_movie_name(idx) for idx in premise)
+    conclusion_name = get_movie_name(conclusion)
+    print "Rule: If a person recommends {0} they will also recommend {1}".format(premise_names, conclusion_name)
+    print " - Confidence: {0:.3f}".format(rule_confidence[(premise, conclusion)])
+    print " "
 
+#评估,训练前200名的用户的打分数据,其他作为测试库
+test_dataset = all_ratings[~all_ratings['UserID'].isin(range(200))]
+test_favorable = test_dataset[test_dataset["Favorable"]]
+test_favorable_by_users = dict((k, frozenset(v.values)) for k, v in test_favorable.groupby("UserID")["MovieID"])
+
+correct_counts = defaultdict(int)
+incorrect_counts = defaultdict(int)
+for user, reviews in test_favorable_by_users.items():
+    for candidate_rule in candidate_rules:
+        premise, conclusion = candidate_rule
+        if premise.issubset(reviews):
+            if conclusion in reviews:
+                correct_counts[candidate_rule] += 1
+            else:
+                incorrect_counts[candidate_rule] += 1
+
+test_confidence = {candidate_rule: correct_counts[candidate_rule] / float(correct_counts[candidate_rule] + incorrect_counts[candidate_rule]) for candidate_rule in rule_confidence}
+print len(test_confidence)
+
+sorted_test_confidence = sorted(test_confidence.items(), key=itemgetter(1), reverse=True)
+for index in range(4,9):
+    print("Rule #{0}".format(index + 1))
+    (premise, conclusion) = sorted_confidence[index][0]
+    premise_names = ", ".join(get_movie_name(idx) for idx in premise)
+    conclusion_name = get_movie_name(conclusion)
+    print "Rule: If a person recommends {0} they will also recommend {1}".format(premise_names, conclusion_name)
+    print " - Train Confidence: {0:.3f}".format(rule_confidence.get((premise, conclusion), -1))
+    print " - Test Confidence: {0:.3f}".format(test_confidence.get((premise, conclusion), -1))
+    print(" ")
