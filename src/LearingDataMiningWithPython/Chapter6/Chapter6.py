5ed67b9233245c7b5e935635b44c34ba1c1be528
caijiahao <634606228@qq.com>
2016-12-09 22:27:17 +0800

    使用朴素贝叶斯进行社会媒体挖掘（二）

--- src/LearingDataMiningWithPython/Chapter6/Chapter6.py
+++ src/LearingDataMiningWithPython/Chapter6/Chapter6.py
@@ -6,11 +6,12 @@
 # @File    : Chapter6.py
 # @Software: PyCharm
 
+#注意：因为是国外网站要用vpn
 import twitter
-consumer_key = "52Nu7ubm2szT1JyJEOB7V2lGM"
-consumer_secret = "mqA94defqjioyWeMxdJsSduthxdMMGd2vfOUKvOFpm0n7JTqfY"
-access_token = "16065520-USf3DBbQAh6ZA8CnSAi6NAUlkorXdppRXpC4cQCKk"
-access_token_secret = "DowMQeXqh5ZsGvZGrmUmkI0iCmI34ShFzKF3iOdiilpX5"
+consumer_key = "GUK5q674gkJDcRh6syRatOcth"
+consumer_secret = "LMBcY7nHkJfQWjZYxo3tAzIOUCfMSHH8yH079RfhmEleiHmxrs"
+access_token = "766306739738271745-Hn3HJm1FnJDEStdavMRBh3T7fzbLUWy"
+access_token_secret = "G0YwIIM9FPOxchd0dEcyu0eoVVlZfgggxstDOwOuzLZbD"
 authorization = twitter.OAuth(access_token, access_token_secret, consumer_key, consumer_secret)
 
 #保存搜索结果
@@ -18,14 +19,16 @@ import os
 output_filename = "python_tweets.json"
 
 import json
-t = twitter.Twitter(auth=authorization)
-with open(output_filename, 'a') as output_file:
-    search_results = t.search.tweets(q="python", count=100)['statuses']
-    for tweet in search_results:
-        if 'text' in tweet:
-            print(tweet['user']['screen_name'])
-            print(tweet['text'])
-            print()
-            output_file.write(json.dumps(tweet))
-            output_file.write("\n\n")
+#t = twitter.Twitter(auth=authorization)
+#with open(output_filename, 'a') as output_file:
+    #search_results = t.search.tweets(q="python", count=100)['statuses']
+    #for tweet in search_results:
+        #if 'text' in tweet:
+            #print(tweet['user']['screen_name'])
+            #print(tweet['text'])
+            #print()
+            #output_file.write(json.dumps(tweet))
+            #output_file.write("\n\n")
+
+
 
