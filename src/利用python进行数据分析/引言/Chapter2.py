a3657075b407e3c073e68f4e69d6f82039279a39
caijiahao <634606228@qq.com>
2017-01-01 22:41:20 +0800

    利用数据分析之引言

--- src/利用python进行数据分析/引言/Chapter2.py
+++ src/利用python进行数据分析/引言/Chapter2.py
@@ -93,11 +93,67 @@ indexer = agg_counts.sum(1).argsort()
 #print indexer[:10]
 
 count_subset = agg_counts.take(indexer)[-10:]
-print count_subset
+#print count_subset
 
 count_subset.plot(kind='barh', stacked=True)
 #show()
 
 normed_subset = count_subset.div(count_subset.sum(1), axis=0)
 normed_subset.plot(kind='barh', stacked=True)
-show()
+#show()
+
+#导入三个数据集
+upath = 'users.dat'
+rpath = 'ratings.dat'
+mpath = 'movies.dat'
+
+unames = ['user_id', 'gender', 'age', 'occupation', 'zip']
+rnames = ['user_id', 'movie_id', 'rating', 'timestamp']
+mnames = ['movie_id', 'title', 'genres']
+
+users = pd.read_csv(upath, sep='::', header=None, names=unames)
+ratings = pd.read_csv(rpath, sep='::', header=None, names=rnames)
+movies = pd.read_csv(mpath, sep='::', header=None, names=mnames)
+
+#print users[:5]
+#print ratings[:5]
+#print movies[:5]
+#print ratings
+
+#合并三张表格
+data = pd.merge(pd.merge(ratings,users),movies)
+#print data
+#print data.ix[0]
+
+#按性别计算电影得分的平均值
+mean_ratings = data.pivot_table('rating', index='title',columns='gender', aggfunc='mean')
+#print mean_ratings[:5]
+
+#赛选评论数大于250条的电影
+ratings_by_title = data.groupby('title').size()
+#print ratings_by_title[:5]
+active_titles = ratings_by_title.index[ratings_by_title >= 250]
+#print active_titles[:10]
+mean_ratings = mean_ratings.ix[active_titles]
+#print mean_ratings
+
+#为了了解女性观众最喜欢的电影，对F列降序排列
+mean_ratings = mean_ratings.rename(index={'Seven Samurai (The Magnificent Seven) (Shichinin no samurai) (1954)':'Seven Samurai (Shichinin no samurai) (1954)'})
+top_female_ratings = mean_ratings.sort_index(by='F', ascending=False)
+#print top_female_ratings[:10]
+
+#计算平均得分之差的列，并对其进行排序
+mean_ratings['diff'] = mean_ratings['M'] - mean_ratings['F']
+sorted_by_diff = mean_ratings.sort_index(by='diff')
+#print sorted_by_diff[:15]
+
+#对排序结果反序得到的就是男性更在喜欢的电影
+print sorted_by_diff[::-1][:15]
+
+#根据电影名称分组得分数据的标准差
+rating_std_by_title = data.groupby('title')['rating'].std()
+#根据active_titles进行过滤
+rating_std_by_title = rating_std_by_title.ix[active_titles]
+#根据值对Series进行降序排列
+#print rating_std_by_title.order(ascending=False)[:10]
+
